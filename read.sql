3660d394fb874f658b37684253be17151cd6032a
Jessica <1049486@cuea.edu>
2024-11-19 11:52:25 +0300

    updates

---
+++ read.sql
@@ -0,0 +1,6 @@
+SELECT * FROM banking_transaction.transaction;
+SELECT * FROM banking_transaction.account;
+SELECT * FROM banking_transaction.employees;
+SELECT * FROM banking_transaction.loan;
+SELECT * FROM banking_transaction.branch;
+
