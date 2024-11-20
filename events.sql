a4bb4e6438b9055d0eba65864c1bcc24e43a9775
Jessica <136574395+JesShiru@users.noreply.github.com>
2024-11-20 15:11:10 +0300

    Events that do updates

---
+++ events.sql
@@ -0,0 +1,15 @@
+-- An event that applies interest to the balance of the AccountType: Savings
+CREATE EVENT IF NOT EXISTS apply_interest
+ON SCHEDULE EVERY 1 MINUTE
+DO
+  UPDATE Accounts
+  SET Balance = Balance + (Balance * 0.01)
+  WHERE AccountType = 'Savings' AND AccountStatus = 'Active';
+
+-- an event that applies interest to the Outstanding loan balance for LoanStatus: Active
+CREATE EVENT IF NOT EXISTS update_outstanding_loan
+ON SCHEDULE EVERY 1 MINUTE
+DO
+    UPDATE Loan
+    SET OutstandingBalance = OutstandingBalance + (OutstandingBalance * (InterestRate / 100) / 12)
+    WHERE LoanStatus = 'Active';
